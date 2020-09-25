8e60999c3cd8f05f9504c4a5ee605d17f741d25a
Ying <LIMX0101@e.ntu.edu.sg>
2020-09-25 14:15:17 +0800

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import admin from './components/admin';
+import signin from './components/signin';
+import logo from './logo.svg'
+import './App.css'
+import Navbar from './components/navbar';
 
-function App() {
+import { Auth } from 'aws-amplify'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <div>
+            <Navbar>
+            <Switch>
+              <Route exact path="/admin" component={admin} />
+              <Route exact path="/signin" component={signin} />
+            </Switch>
+            </Navbar>
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+/*function checkUser() {
+  Auth.currentAuthenticatedUser()
+    .then(user => console.log({ user }))
+    .catch(err => console.log(err))
+}
+
+function signOut() {
+  Auth.signOut()
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+}
+
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
+        <button onClick={checkUser}>Check User</button>
+        <button onClick={signOut}>Sign Out</button>
       </header>
     </div>
-  );
-}
+  )
+}*/
 
-export default App;
+export default App
